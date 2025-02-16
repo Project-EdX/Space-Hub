d78003728dafdc7a29c30fc598fb3fb929e8ccf5
Sagar <134053227+Sagar2-1o@users.noreply.github.com>
2025-02-16 14:00:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  // optimizeDeps: {
+  //   exclude: ['lucide-react'],
+  // },
   server: {
-    allowedHosts: ['hnsmk9-5173.csb.app'],
+    allowedHosts: 'all',
   },
 });
