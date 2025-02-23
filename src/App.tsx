ad86a69340e6c3c056a09400b18634868457789b
Sagar <134053227+Sagar2-1o@users.noreply.github.com>
2025-02-23 19:20:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { AppRouter } from "../src/Routers/Routes";
 
-// export const supabase = createClient(
-//   import.meta.env.VITE_SUPABASE_URL as string,
-//   import.meta.env.VITE_SUPABASE_ANON_KEY as string,
-// );
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL as string,
+  import.meta.env.VITE_SUPABASE_ANON_KEY as string,
+);
 
 function App() {
   return (
